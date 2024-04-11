a7308ccf75a05153382b2660239e0fc607131f9a
luxxaa12 <159189844+luxxaa12@users.noreply.github.com>
2024-04-11 18:08:24 +0000

    exe

---
+++ Exercicio em aula .py
@@ -0,0 +1,13 @@
+import os
+
+os.system("clear")
+
+numero:int
+
+numero= int(input("Digite um numero:"))
+
+for i in range (0,11):
+    print(f"{numero} * {i} = {numero * i} ")
+numero 
+
+
---
+++ Exercicios Guanabara/Exercicio 7.py
@@ -0,0 +1,17 @@
+import os
+os.system("clear")
+
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+km : int
+metros : int
+decam : int
+hecto : int
+deci : float
+cent : float
+mili : int
+
+
+
+
+
